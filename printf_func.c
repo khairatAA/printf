b401e8390edd7e24a1680d7af5d22560cb7e98b2
Ruky8821 <rumena78@gmail.com>
2023-05-27 13:51:53 -0700

    printf_func.c updated

--- printf_func.c
+++ printf_func.c
@@ -17,13 +17,36 @@ int _printf(const char *format, ...)
 	i = 0;
 	count_tmp = 0;
 	count = 0;
+	va_list spec
+	va_start(spec, format);
+
 	if (format == NULL)
 		return (-1);
 	while (format[i])
 	{
-		count_tmp = write(1, &format[i], 1);
-		count += count_tmp;
-		i++;
+		if (format[i] != '%' || format[i] != '\0') /* not % and not end of string*/
+		{
+			count_tmp = write(1, &format[i], 1);
+			count += count_tmp;
+			i++;
+			continue;
+		}
+		if (format[i] == '%')
+		{
+			p = get_spec_func(format[i + 1]);
+			/*chechking if end of file has been reached*/
+			/**
+			 *  if (p == '\0')
+			 *	break;
+			 *
+			 *if (p != '\0')
+			 *{
+			 *	count_tmp = write(1, &format[i], 1);
+			 *	count += count_tmp;
+			 *	i++;
+			 *}
+			 */
+		}
 	}
 
 	return (count);
