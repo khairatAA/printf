cb023ade3b1bbe9eafbd8801005a2784960da962
Ruky8821 <rumena78@gmail.com>
2023-05-27 17:18:13 -0700

    printf_func.c modified to recognise %c

--- printf_func.c
+++ printf_func.c
@@ -12,20 +12,18 @@
  */
 int _printf(const char *format, ...)
 {
-	int count, i, count_tmp, p;
-
-	i = 0;
-	count_tmp = 0;
-	count = 0;
 	va_list spec;
+
 	va_start(spec, format);
 	int (*f)(va_list);
+	int i = 0, count_tmp = 0;
+	int count = 0;
 
 	if (format == NULL)
 		return (-1);
 	while (format[i])
 	{
-		if (format[i] != '%' || format[i] != '\0') /* not % and not end of string*/
+		if (format[i] != '%') /* not % and not end of string*/
 		{
 			count_tmp = write(1, &format[i], 1);
 			count += count_tmp;
@@ -34,9 +32,24 @@ int _printf(const char *format, ...)
 		}
 		if (format[i] == '%')
 		{
-			f = get_spec_func(format[i + 1]);
+			f = get_spec_func(&format[i + 1]);
+			if (f != NULL)
+			{
+				count_tmp = f(spec);
+				count += count_tmp;
+				i += 2;
+				continue;
+			}
+			if (format[i + 1] != '\0')
+			{
+				count_tmp = write(1, &format, 1);
+				count += count_tmp;
+				i++;
+				continue;
+			}
+			if (format[i + 1] == '\0')
+				break;
 		}
 	}
-
 	return (count);
 }
