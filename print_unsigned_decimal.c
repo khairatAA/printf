58cd0363b0a9c44ddefabb406bedb78b86a20b82
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-29 07:17:54 -0700

    fixed the error in print_unsigned_decimal.c file

--- print_unsigned_decimal.c
+++ print_unsigned_decimal.c
@@ -8,32 +8,18 @@
  */
 int print_unsigned_decimal(va_list spec)
 {
-	int u = va_arg(spec, int);
+	unsigned int u = va_arg(spec, unsigned int);
 	int count, i;
 	char dec_arr[12];
 
 	count = 0;
 	if (u)
 	{
-		if (u < 0)
+		i = 0;
+		while (u > 0)
 		{
-			u = -1 * (u);
-			write(1, &u, 1);
-			count++;
-		}
-		if (u == 0)
-		{
-			write(1, "0", 1);
-			count++;
-		}
-		else
-		{
-			i = 0;
-			while (u > 0)
-			{
-				dec_arr[i++] = u % 10 + '0';
-				u = u / 10;
-			}
+			dec_arr[i++] = u % 10 + '0';
+			u = u / 10;
 		}
 		while (i > 0)
 		{
@@ -43,5 +29,8 @@ int print_unsigned_decimal(va_list spec)
 		return (count);
 	}
 	else
-		return (0);
+	{
+		write(1, "0", 1);
+		return (1);
+	}
 }
