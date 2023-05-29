e551654be1b1dd930cabda3a093694672657919a
Ruky8821 <rumena78@gmail.com>
2023-05-28 17:24:12 -0700

    debugging

--- print_binary.c
+++ print_binary.c
@@ -10,11 +10,10 @@
 int print_binary(va_list spec)
 {
 	unsigned int b;
-	int i, count, count2;
-	int a[32];
+	int i, count;
+	int a[16];
 
 	count = 0;
-	count2 = 0;
 	b = va_arg(spec, int);
 	if (b)
 	{
@@ -22,12 +21,12 @@ int print_binary(va_list spec)
 		{
 			a[i] = b % 2;
 			b = b / 2;
-			count2 += 1;
 		}
 
-		for (i = count2 - 1; i >= 0; i--)
+		while (i > 0)
 		{
-			count = write(1, &a[i], 1);
+			write(1, &a[--i], 1);
+			count++;
 		}
 		return (count);
 	}
