a31374e57f078b7bfaa23cba44fa9745794ab0a1
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-30 14:35:57 -0700

    print_address.c file updated

--- print_address.c
+++ print_address.c
@@ -8,22 +8,26 @@
 int print_address(va_list spec)
 {
 	void *a = va_arg(spec, void *);
-	char buffer[1024];
-	int buffer_i = sizeof(buffer), i = buffer_i - 2, count = 2;
+	char buffer[1024], hex_digit;
+	int buffer_i = sizeof(buffer), num_digits = 0, len, i;
 	unsigned long num_address;
-	char hex[] = "0123456789abcdef";
 
 	if (a == NULL)
 		return (write(1, "(nil)", 5));
-	buffer[buffer_i - 1] = '\0';
 	num_address = (unsigned long)a;
-	while(num_address > 0)
+	while (num_address > 0)
 	{
-		buffer[i--] = hex[num_address % 16];
-		num_address = num_address / 16;
-		count++;
+		num_address /= 16;
+		num_digits++;
 	}
-	i++;
+	num_digits = (num_digits == 0) ? 1 : num_digits;
+	len = snprintf(buffer, buffer_i, "0x");
+	for (i = (num_digits - 1) * 4; i >= 0; i -= 4)
+	{
+		hex_digit = (unsigned long)((unsigned long)a >> i) & 0xF;
+		buffer[len++] = (hex_digit < 10) ? ('0' + hex_digit) :
+			('a' + hex_digit - 10);
+	}
 
-	return (write(1, &buffer[i], buffer_i - i - 1));
+	return (write(1, buffer, len));
 }
