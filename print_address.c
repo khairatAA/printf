470ff94647d3f2497efad712e9707f8b3a2c188d
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-30 13:14:22 -0700

    updated print_address.c the file

--- print_address.c
+++ print_address.c
@@ -7,7 +7,23 @@
  */
 int print_address(va_list spec)
 {
-	int count = va_arg(spec, signed int);
+	void *a = va_arg(spec, void *);
+	char buffer[1024];
+	int buffer_i = sizeof(buffer), i = buffer_i - 2, count = 2;
+	unsigned long num_address;
+	char hex[] = "0123456789abcdef";
 
-	return (count);
+	if (a == NULL)
+		return (write(1, "(nil)", 5));
+	buffer[buffer_i - 1] = '\0';
+	num_address = (unsigned long)a;
+	while(num_address > 0)
+	{
+		buffer[i--] = hex[num_address % 16];
+		num_address = num_address / 16;
+		count++;
+	}
+	i++;
+
+	return (write(1, &buffer[i], buffer_i - i - 1));
 }
--- test/test.c
+++ test/test.c
@@ -11,6 +11,7 @@ int main(void)
 {
 	int count_our_pf = 0;
 	int count_pf = 0;
+	void* address = (void*)0x12345678;
 
 	write(1, "NORMAL PRINTF:\n", 15);
 	count_pf = printf("Character:[%c]%c\n", 't','R');
@@ -18,6 +19,17 @@ int main(void)
 	write(1, "--------------------------------------------\n", 45);
 	write(1, "OUR PRINTF:\n", 12);
 	count_our_pf = _printf("Character:[%c]%c\n",'t','R');
+	printf("The total count printed on our printf is %d\n", count_our_pf);
+
+	write(1, "--------------------------------------------\n", 45);
+	write(1, "--------------------------------------------\n", 45);
+
+	write(1, "NORMAL PRINTF:\n", 15);
+	count_pf = printf("Address:[%p]\n", address);
+	printf("The total count printed on printf is %d\n", count_pf);
+	write(1, "--------------------------------------------\n", 45);
+	write(1, "OUR PRINTF:\n", 12);
+	count_our_pf = _printf("Address:[%p]\n", address);
 	printf("The total count printed on our printf is %d\n", count_our_pf);
 
 	return (0);
